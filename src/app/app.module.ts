c177a1daaef8a274775ee7c58b052e8fbf6dda5f
Shivam Singh <sivamsingh7762@gmail.com>
2023-07-30 15:01:45 +0530

    compleated footer part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,9 @@ import { GetProductService } from './services/get-product.service';
 import { ProductCardComponent } from './component/product-card/product-card.component';
 import { JournalCardComponent } from './component/journal-card/journal-card.component';
 import { AttributeGridComponent } from './component/attribute-grid/attribute-grid.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, TitleComponent, ProductCardComponent, JournalCardComponent, AttributeGridComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, TitleComponent, ProductCardComponent, JournalCardComponent, AttributeGridComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [GetProductService],
   bootstrap: [AppComponent],
